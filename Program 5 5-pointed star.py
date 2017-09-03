ba3940d43383d748259ce1ad838a6976230889e8
ricklinz <zachary.ricklin@gmail.com>
2017-09-03 19:54:25 -0400

    Add files via upload

---
+++ Program 5 5-pointed star.py
@@ -0,0 +1,10 @@
+#Name: Zachary Ricklin
+#Date: September 2, 2017
+# This program prints: a five pointed star
+
+import turtle
+	Ross = turtle.Turtle()
+  
+for i in range(5):
+  Ross.forward(100)
+  Ross.right(144)
